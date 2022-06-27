05aeea42f47d4fbfd03d385340b2a3075015e241
apurihsu@gmail.com <apurihsu@gmail.com>
2022-06-27 23:49:38 +0900

    Initial commit

---
+++ app/src/main/java/example/com/animalbook/MainActivity.kt
@@ -0,0 +1,21 @@
+package example.com.animalbook
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.ktx.Firebase
+import example.com.animalbook.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.nextButton.setOnClickListener {
+            val intent = Intent(this, SubActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
